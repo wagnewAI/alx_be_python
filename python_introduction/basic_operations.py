7fd325f80a567d782c7217291cf449fa80d35c63
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-07-12 12:30:24 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,11 @@
+# Assign values to variables
+number1 = 10
+number2 = 5
 
+# Perform arithmetic operations
+addition = number1 + number2
+print("Addition of", number1, "and", number2, "is", addition)
+subtraction = number1 - number2
+print("Subtraction of", number1, "and",number2, "is", subtraction)
+multiplication = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", multiplication)
