3ca14e5e468e7bce286e6ef203e8b5b75229c2e7
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-07-25 10:05:41 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,5 @@
-def perform_operation(num1: float, num2: float, operation: str):
+#def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Performs basic arithmetic operations: add, subtract, multiply, divide.
 
