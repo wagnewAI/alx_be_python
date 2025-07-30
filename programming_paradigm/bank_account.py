dd7aabe112cfe9a689aee290c5e1289eb4a5f296
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-07-30 10:22:16 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,1 +1,21 @@
 
+class BankAccount:
+    def __init__(self, initial_balance=0.0):
+        self.__account_balance = float(initial_balance)
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            print("Insufficient funds.")
+            return False
+
+    def display_balance(self):
+        print(f"Current balance: ${self.__account_balance:.2f}")
