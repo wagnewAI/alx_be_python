cf49bc5dac457aec8d3079c6574cba83812b62f5
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-07-30 14:59:47 +0300

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -10,4 +10,4 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."
 
     except ValueError:
-        return "Error: Inputs must be numeric."
+        return "Error: Please enter numeric values only."
